5972946476300b9c99dc397ebe2f912575801c7c
友流 郑 <zhengyouliu@outlook.com>
2019-05-04 14:51:34 +0800

    training middlewaredemo

---
+++ MiddlewareDemo/MiddlewareDemo/MiddleWareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using MiddlewareDemo.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDemo
+{
+    public static class MiddleWareExtensions
+    {
+        public static IApplicationBuilder UseRequestIp(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestIpMiddleware>();
+        }
+    }
+}
---
+++ MiddlewareDemo/MiddlewareDemo/Middleware/RequestIpMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDemo.Middleware
+{
+    public class RequestIpMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestIpMiddleware(RequestDelegate next,ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestIpMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation("User Ip:" + context.Connection.RemoteIpAddress.ToString());
+            await _next.Invoke(context);
+        }
+    }
+}
